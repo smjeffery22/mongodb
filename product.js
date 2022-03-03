27ba5169f9b53e304369f800b1045952faa48e0d
smjeffery22 <parkjon8@gmail.com>
2022-03-03 11:00:46 -0500

    Practice instance and static methods

--- product.js
+++ product.js
@@ -34,30 +34,62 @@ const productSchema = new mongoose.Schema({
 			default: 0,
 		},
 	},
-  size: {
-    type: String,
-    enum: ['S', 'M', 'L']
-  }
+	size: {
+		type: String,
+		enum: ['S', 'M', 'L'],
+	},
 });
+
+// productSchema.methods.greet = function () {
+// 	console.log('Hello');
+//   console.log(`- from ${this.name}`)
+// };
+
+productSchema.methods.toggleOnSale = function () {
+	this.onSale = !this.onSale;
+	return this.save();
+};
+
+productSchema.methods.addCategory = function (newCat) {
+	this.categories.push(newCat);
+	return this.save();
+};
+
+productSchema.statics.fireSale = function () {
+	return this.updateMany({}, { onSale: true, price: 0 });
+};
 
 const Product = mongoose.model('Product', productSchema);
 
-const bike = new Product({
-	name: 'Cycling Jersey',
-	price: 28.50,
-	categories: ['Cycling'],
-  size: 'M'
-});
-bike
-	.save()
-	.then((data) => {
-		console.log('Added');
-		console.log(data);
-	})
-	.catch((err) => {
-		console.log('Error');
-		console.log(err);
-	});
+const findProduct = async () => {
+	const foundProduct = await Product.findOne({ name: 'Bike Helmet' });
+	console.log(foundProduct);
+	await foundProduct.toggleOnSale();
+	console.log(foundProduct);
+	await foundProduct.addCategory('Outdoors');
+	console.log(foundProduct);
+};
+
+Product.fireSale().then(res => console.log(res));
+
+// findProduct();
+
+// const bike = new Product({
+// 	name: 'Cycling Jersey',
+// 	price: 28.50,
+// 	categories: ['Cycling'],
+//   size: 'M'
+// });
+// bike
+// 	.save()
+// 	.then((data) => {
+// 		console.log('Added');
+// 		console.log(data);
+// 	})
+// 	.catch((err) => {
+// 		console.log('Error');
+// 		console.log(err);
+// 	});
 
 // Product.findOneAndUpdate({ name: 'Tire Pump' }, { price: -19.5 }, { new: true, runValidators: true })
 // 	.then((data) => {
