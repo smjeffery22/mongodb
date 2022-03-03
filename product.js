ee08e57ea8b6f9ad071159ab7d0b35d8eda07cba
smjeffery22 <parkjon8@gmail.com>
2022-03-03 10:26:05 -0500

    More practice with validations'

--- product.js
+++ product.js
@@ -9,22 +9,62 @@ const productSchema = new mongoose.Schema({
 	name: {
 		type: String,
 		required: true,
+		maxlength: 20,
 	},
 	price: {
 		type: Number,
-    required: true
+		required: true,
+		min: [0, 'Price cannot be negative'],
 	},
+	onSale: {
+		type: Boolean,
+		default: false,
+	},
+	categories: {
+		type: [String],
+		default: ['Cycling'],
+	},
+	qty: {
+		online: {
+			type: Number,
+			default: 0,
+		},
+		inStore: {
+			type: Number,
+			default: 0,
+		},
+	},
+  size: {
+    type: String,
+    enum: ['S', 'M', 'L']
+  }
 });
 
 const Product = mongoose.model('Product', productSchema);
 
-const bike = new Product({ name: 'Mountain Bike', price: '599', color: 'red' });
-bike.save()
-  .then(data => {
-    console.log('Bike added');
-    console.log(data);
-  })
-  .catch(err => {
-    console.log('Error');
-    console.log(err);
-  })
+const bike = new Product({
+	name: 'Cycling Jersey',
+	price: 28.50,
+	categories: ['Cycling'],
+  size: 'M'
+});
+bike
+	.save()
+	.then((data) => {
+		console.log('Added');
+		console.log(data);
+	})
+	.catch((err) => {
+		console.log('Error');
+		console.log(err);
+	});
+
+// Product.findOneAndUpdate({ name: 'Tire Pump' }, { price: -19.5 }, { new: true, runValidators: true })
+// 	.then((data) => {
+// 		console.log('Added');
+// 		console.log(data);
+// 	})
+// 	.catch((err) => {
+// 		console.log('Error');
+// 		console.log(err);
+// 	});
